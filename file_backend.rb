833b9140ebff8a03e82b362c394a9ab81ab013b5
John (Jack) Brown <jack@brownjohnf.com>
2015-10-04 17:56:19 -0700

    Almost finished with filebackend specs; broken database specs

--- file_backend.rb
+++ file_backend.rb
@@ -253,12 +253,25 @@ module S3DB
       File.join(@path, db_name, collection_name, 'data', filename)
     end
 
+    # Write a directory to disk for the name of the database. Will fail if the
+    # database already exists.
+    #
+    # db_name   - String name of database. Required.
+    #
+    # returns db_name on success; raises an error on failure.
     def write_db(db_name)
       Dir.mkdir(db_path(db_name))
 
       db_name
     end
 
+    # Write a directory to disk for the name of the collection. Will ignore the
+    # write if the directory exists.
+    #
+    # db_name           - String name of database. Required.
+    # collection_name   - String name of collection. Required.
+    #
+    # returns collection_name on success; raises an error on failure.
     def write_collection(db_name, collection_name)
       dir = collection_path(db_name, collection_name)
 
@@ -275,62 +288,165 @@ module S3DB
       collection_name
     end
 
-    def write_schema(db_name, collection_name, json_schema)
+    # Write a file to disk for the schema for a database/collection.
+    #
+    # db_name           - String name of database. Required.
+    # collection_name   - String name of collection. Required.
+    # schema            - String schema contents. Required.
+    #
+    # returns the schema on success; raises an error on failure.
+    def write_schema(db_name, collection_name, schema)
       File.open(schema_path(db_name, collection_name), 'w') do |f|
-        f.puts json_schema
+        f.puts schema
       end
 
-      true
+      schema
     end
 
-    def read_schema(db_name, collection_name)
-      file = schema_path(db_name, collection_name)
+    # Write a record to a database/collection. The record can be in any format.
+    # No parsing of the file is performed. Data must be sent as a string.
+    #
+    # db_name           - String name of database. Required.
+    # collection_name   - String name of collection. Required.
+    # filename          - String name of file to write, w/extension. Required.
+    # data              - String data to write to the file. Required.
+    #
+    # returns the data written.
+    def write_record(db_name, coll_name, filename, data)
+      raise ArgumentError, 'data must be a string!' unless data.is_a?(String)
 
-      File.read(file)
+      File.open(record_path(db_name, coll_name, filename), 'w') do |f|
+        f.puts data
+      end
+
+      data
     end
 
+    # List all available collections in a database.
+    #
+    # db_name   - String name of database to look in. Required.
+    #
+    # returns an Array of String collection names.
+    def list_collections(db_name)
+      Dir.entries(db_path(db_name)).select do |dir|
+        !File.directory?(dir)
+      end
+    end
+
+    # List all available records for a database/collection.
+    #
+    # db_name           - String name of database to look in. Required.
+    # collection_name   - String name of collection to look in. Required.
+    #
+    # returns an Array of String record file names.
     def list_records(db_name, coll_name)
       output = []
 
       Dir.open(data_path(db_name, coll_name)) do |dir|
         dir.each do |file|
-          output << File.read(File.join(dir.path, file)) unless File.directory?(file)
+          output << file unless File.directory?(file)
         end
       end
 
       output
     end
 
-    def save_record(db_name, coll_name, filename, data)
-      File.open(record_path(db_name, coll_name, filename), 'w') do |f|
-        f.puts data
+    # Read the contents of the schema file.
+    #
+    # db_name           - String name of database to look in. Required.
+    # collection_name   - String name of collection to look in. Required.
+    #
+    # returns the String contents of the schema file. Raises an error if the
+    # file is missing.
+    def read_schema(db_name, collection_name)
+      file = schema_path(db_name, collection_name)
+
+      begin
+        File.read(file)
+      rescue Errno::ENOENT
+        raise ArgumentError, 'schema does not exist!'
       end
     end
 
+    # Read the contents of a record in a database/collection
+    #
+    # db_name           - String name of database to look in. Required.
+    # collection_name   - String name of collection to look in. Required.
+    # filename          - String name of the file to read w/extension. Required.
+    #
+    # returns the String contents of the record file. Raises an error if the
+    # file is missing.
     def read_record(db_name, coll_name, filename)
-      File.read(record_path(db_name, coll_name, filename))
+      file = record_path(db_name, coll_name, filename)
+
+      begin
+        File.read(file)
+      rescue Errno::ENOENT
+        raise ArgumentError, 'record does not exist!'
+      end
     end
 
+    # Delete a database directory from disk. Will only succed if the database is
+    # empty.
+    #
+    # db_name           - String name of database to remove. Required.
+    #
+    # returns an Array of the databases deleted, or empty if the database did
+    # not exist.
     def delete_db(db_name)
-      FileUtils.rm_rf(db_path(db_name))
+      path = db_path(db_name)
 
-      db_name
+      if Dir.exist?(path)
+        begin
+          Dir.rmdir(path)
+        rescue Errno::ENOTEMPTY
+          raise ArgumentError, 'database is not empty!'
+        end
+
+        return [db_name]
+      end
+
+      []
     end
 
+    # Delete a collection directory from disk. Will only succed if the
+    # collection is empty.
+    #
+    # db_name           - String name of database to remove. Required.
+    # collection_name   - String name of collection to remove. Required.
+    #
+    # returns an Array of the databases deleted, or empty if the database did
+    # not exist.
     def delete_collection(db_name, collection_name)
-      FileUtils.rm_rf(collection_path(db_name, collection_name))
+      path = collection_path(db_name, collection_name)
 
-      collection_name
+      if File.exist?(schema_path(db_name, collection_name))
+        File.delete(schema_path(db_name, collection_name))
+      end
+
+      if Dir.exist?(data_path(db_name, collection_name))
+        begin
+          Dir.rmdir(data_path(db_name, collection_name))
+        rescue Errno::ENOTEMPTY
+          raise ArgumentError, 'collection/data is not empty!'
+        end
+      end
+
+      if Dir.exist?(path)
+        begin
+          Dir.rmdir(path)
+        rescue Errno::ENOTEMPTY
+          raise ArgumentError, 'collection is not empty!'
+        end
+
+        return [collection_name]
+      end
+
+      []
     end
 
     def valid_db?(db_name)
       Dir.exist?(db_path(db_name))
-    end
-
-    def list_collections(db_name)
-      Dir.entries(db_path(db_name)).select do |dir|
-        dir != '.' && dir != '..'
-      end
     end
 
     def storage_location(db_name)
--- spec/file_backend_spec.rb
+++ spec/file_backend_spec.rb
@@ -54,6 +54,10 @@ RSpec.describe S3DB::FileBackend do
       S3DB::FileBackend.new(TEST_DB_BASE_PATH)
     end
 
+    let(:dbname) { 'testdb' }
+    let(:coll_name) { 'testcoll' }
+    let(:record_name) { 'testrec' }
+
     describe '#validate_path' do
       before :each do
         subject.instance_variable_set(:@errors, [])
@@ -258,6 +262,282 @@ RSpec.describe S3DB::FileBackend do
       it 'builds a path from the base path, db name, coll name and filename' do
         expect(subject.record_path('test', 'coll', 'file.json')).to \
           eq "#{TEST_DB_BASE_PATH}/test/coll/data/file.json"
+      end
+    end
+
+    describe '#write_db' do
+      after :each do
+        S3DB::FileBackend.delete(TEST_DB_BASE_PATH)
+      end
+
+      it 'creates a db dir' do
+        expect(Dir.exist?(TEST_DB_BASE_PATH + '/' + dbname)).to be false
+
+        subject.save
+        expect(subject.write_db(dbname)).to eq dbname
+
+        expect(Dir.exist?(TEST_DB_BASE_PATH + '/' + dbname)).to be true
+      end
+
+      it 'raises an error if the path already exists' do
+        subject.save
+        subject.write_db(dbname)
+        expect(Dir.exist?(TEST_DB_BASE_PATH + '/' + dbname)).to be true
+
+        expect { subject.write_db(dbname) }.to raise_error Errno::EEXIST
+      end
+    end
+
+    describe '#write_collection' do
+      before :each do
+        subject.save
+        subject.write_db(dbname)
+      end
+
+      after :each do
+        S3DB::FileBackend.delete(TEST_DB_BASE_PATH)
+      end
+
+      let(:path) { File.join(TEST_DB_BASE_PATH, dbname, coll_name) }
+
+      it 'creates a collection dir' do
+        expect(Dir.exist?(path)).to be false
+
+        expect(subject.write_collection(dbname, coll_name)).to eq coll_name
+
+        expect(Dir.exist?(path)).to be true
+      end
+
+      it 'creates a data dir' do
+        expect(Dir.exist?(File.join(path, 'data'))).to be false
+
+        expect(subject.write_collection(dbname, coll_name)).to eq coll_name
+
+        expect(Dir.exist?(File.join(path, 'data'))).to be true
+      end
+
+      it 'raises an error if the db does not exist' do
+        S3DB::FileBackend.delete(TEST_DB_BASE_PATH)
+        expect(Dir.exist?(path)).to be false
+
+        expect { subject.write_collection(dbname, coll_name) }.to \
+          raise_error Errno::ENOENT
+      end
+
+      it 'does not raise error if the dir already exists' do
+        subject.write_collection(dbname, coll_name)
+        expect(Dir.exist?(path)).to be true
+
+        expect { subject.write_collection(dbname, coll_name) }.to_not raise_error
+      end
+    end
+
+    describe '#write_schema' do
+      before :each do
+        subject.save
+        subject.write_db(dbname)
+        subject.write_collection(dbname, coll_name)
+      end
+
+      after :each do
+        S3DB::FileBackend.delete(TEST_DB_BASE_PATH)
+      end
+
+      let(:path) { File.join(TEST_DB_BASE_PATH, dbname, coll_name, 'schema.json') }
+
+      it 'creates a schema' do
+        expect(File.exist?(path)).to be false
+
+        expect(subject.write_schema(dbname, coll_name, '{}')).to eq '{}'
+
+        expect(File.exist?(path)).to be true
+      end
+
+      it 'does not raise an error if the schema already exists' do
+        subject.write_schema(dbname, coll_name, '{}')
+        expect(File.exist?(path)).to be true
+
+        expect { subject.write_schema(dbname, coll_name, '{}') }.to_not raise_error
+      end
+    end
+
+    describe '#write_record' do
+      before :each do
+        subject.save
+        subject.write_db(dbname)
+        subject.write_collection(dbname, coll_name)
+      end
+
+      after :each do
+        S3DB::FileBackend.delete(TEST_DB_BASE_PATH)
+      end
+
+      let(:path) do
+        File.join(TEST_DB_BASE_PATH, dbname, coll_name, 'data', record_name)
+      end
+
+      it 'creates a record' do
+        expect(File.exist?(path)).to be false
+
+        expect(subject.write_record(dbname, coll_name, record_name, '{}')).to \
+          eq '{}'
+
+        expect(File.exist?(path)).to be true
+      end
+
+      it 'overwrites the record if it exists' do
+        subject.write_record(dbname, coll_name, record_name, '{test: true}')
+        expect(File.exist?(path)).to be true
+        expect(File.read(path)).to eq "{test: true}\n"
+
+        expect do
+          subject.write_record(dbname, coll_name, record_name, '{}')
+        end.to_not raise_error
+
+        expect(File.read(path)).to eq "{}\n"
+      end
+
+      it 'raises an error if the data is not a string' do
+        expect do
+          subject.write_record(dbname, coll_name, record_name, 1)
+        end.to raise_error ArgumentError, 'data must be a string!'
+      end
+    end
+
+    describe '#list_collections' do
+      before :each do
+        subject.save
+        subject.write_db(dbname)
+      end
+
+      after :each do
+        S3DB::FileBackend.delete(TEST_DB_BASE_PATH)
+      end
+
+      it 'returns an array of directories' do
+        expect(subject.list_collections(dbname)).to eq []
+        subject.write_collection(dbname, coll_name)
+        expect(subject.list_collections(dbname)).to eq [coll_name]
+      end
+    end
+
+    describe '#list_records' do
+      before :each do
+        subject.save
+        subject.write_db(dbname)
+        subject.write_collection(dbname, coll_name)
+      end
+
+      after :each do
+        S3DB::FileBackend.delete(TEST_DB_BASE_PATH)
+      end
+
+      it 'returns an array of files' do
+        expect(subject.list_records(dbname, coll_name)).to eq []
+        subject.write_record(dbname, coll_name, record_name, '{}')
+        expect(subject.list_records(dbname, coll_name)).to eq [record_name]
+      end
+    end
+
+    describe '#read_schema' do
+      before :each do
+        subject.save
+        subject.write_db(dbname)
+        subject.write_collection(dbname, coll_name)
+      end
+
+      after :each do
+        S3DB::FileBackend.delete(TEST_DB_BASE_PATH)
+      end
+
+      it 'returns the contents of the schema file' do
+        subject.write_schema(dbname, coll_name, '{test: true}')
+        expect(subject.read_schema(dbname, coll_name)).to eq "{test: true}\n"
+      end
+
+      it 'raises an error if the schema is missing' do
+        expect do
+          subject.read_schema(dbname, coll_name)
+        end.to raise_error ArgumentError, 'schema does not exist!'
+      end
+    end
+
+    describe '#read_record' do
+      before :each do
+        subject.save
+        subject.write_db(dbname)
+        subject.write_collection(dbname, coll_name)
+      end
+
+      after :each do
+        S3DB::FileBackend.delete(TEST_DB_BASE_PATH)
+      end
+
+      it 'returns the contents of the record' do
+        subject.write_record(dbname, coll_name, record_name, '{test: true}')
+        expect(subject.read_record(dbname, coll_name, record_name)).to \
+          eq "{test: true}\n"
+      end
+
+      it 'raises an error if the record is missing' do
+        expect do
+          subject.read_record(dbname, coll_name, record_name)
+        end.to raise_error ArgumentError, 'record does not exist!'
+      end
+    end
+
+    describe '#delete_db' do
+      before :each do
+        subject.save
+        subject.write_db(dbname)
+      end
+
+      after :each do
+        S3DB::FileBackend.delete(TEST_DB_BASE_PATH)
+      end
+
+      it 'deletes an empty db' do
+        expect(subject.delete_db(dbname)).to eq [dbname]
+      end
+
+      it 'raises an error if the directory is not empty' do
+        subject.write_collection(dbname, coll_name)
+
+        expect { subject.delete_db(dbname) }.to raise_error 'database is not empty!'
+      end
+
+      it 'returns an empty array if there was no db' do
+        subject.delete_db(dbname)
+        expect(subject.delete_db(dbname)).to eq []
+      end
+    end
+
+    describe '#delete_collection' do
+      before :each do
+        subject.save
+        subject.write_db(dbname)
+        subject.write_collection(dbname, coll_name)
+      end
+
+      after :each do
+        S3DB::FileBackend.delete(TEST_DB_BASE_PATH)
+      end
+
+      it 'deletes an empty collection' do
+        expect(subject.delete_collection(dbname, coll_name)).to eq [coll_name]
+      end
+
+      it 'raises an error if the directory is not empty' do
+        subject.write_record(dbname, coll_name, record_name, '{}')
+
+        expect do
+          subject.delete_collection(dbname, coll_name)
+        end.to raise_error 'collection/data is not empty!'
+      end
+
+      it 'returns an empty array if there was no collection' do
+        subject.delete_collection(dbname, coll_name)
+        expect(subject.delete_collection(dbname, coll_name)).to eq []
       end
     end
   end
